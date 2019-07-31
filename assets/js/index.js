4a4eb223bef9d409ce612840d5685b2f59807081
AntiHero <prophreaker@gmail.com>
2019-07-31 16:05:31 +0300

    full nav add + nav animation effect add(feat)

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,23 @@
 console.log('hello');
+
+$(document).ready(() => {
+  $('.nav-icon').click(() => {
+    $('.full-nav-wrapper').addClass('open-nav');
+    $('body').css('overflow', 'hidden');
+  })
+
+  $('.full-nav__close').click(() => {
+    $('.full-nav-wrapper').removeClass('open-nav');
+    $('body').css('overflow-y', 'scroll');
+  })
+
+  $(window).scroll(() => {
+    let sc = $(window).scrollTop();
+    console.log(sc);
+     if (sc > 100) {
+       $('.nav').addClass('nav--sticked');
+     } else {
+       $('.nav').removeClass('nav--sticked');
+     }
+  })
+})
