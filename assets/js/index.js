0856c3a2c8b07522dfb436fbd12906e90a5a5886
AntiHero <prophreaker@gmail.com>
2019-08-06 18:04:32 +0300

    main slider resizing bug fix(fix)

--- assets/js/index.js
+++ assets/js/index.js
@@ -23,30 +23,35 @@ $(document).ready(() => {
   const slider = document.querySelector('.main-section-slider');
   const controls = document.querySelectorAll('.slider-control');
   let currentSlide = 0;
+  let currentControlIndex = null;
   let control = controls[currentSlide];
   
   $(slider).click((e) => {
     e.preventDefault();
     e.stopPropagation();
-    let currentControlIndex = null;
     
     for (let i = 0; i < control.children.length; i++) {
       if (e.target === control.children[i]) {
         currentControlIndex = i;
-        //const root = document.documentElement;
         const w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-        //root.style.setProperty('--containerWidth', w + "px");
-        console.log(w);
         const xOffset = currentControlIndex*w;
 
         for (let i = 0; i < control.children.length; i++) {
           $(slider.children[i]).css({'transition' : 'transform 1.5s ease-in-out','transform' : `translateX(${-xOffset}px)`});
         }
-    
+
         currentSlide = currentControlIndex;
         control = controls[currentSlide];
       } 
     }
   })
-  console.log(control.children[0]);
+
+  $(window).resize((e) => {
+    const w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    const xOffset = currentControlIndex*w;
+
+    for (let i = 0; i < control.children.length; i++) {
+      $(slider.children[i]).css({'transition' : 'transform 0s ease-in-out','transform' : `translateX(${-xOffset}px)`});
+    }
+  })
 })
